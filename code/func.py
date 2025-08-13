08f454ba743bdcccb0c16ac13fe99ab5b52760cd
Quốc Việt (Vision) Nguyễn <132635583+visiondrag@users.noreply.github.com>
2025-08-13 11:00:28 +0900

    a

--- code/func.py
+++ code/func.py
@@ -1,17 +1,4 @@
 import os
 
-def list_current_folder():
-    current_dir = os.getcwd()
-    result = {
-        "current_directory": current_dir,
-        "directories": [],
-        "files": []
-    }
-
-    for item in os.listdir(current_dir):
-        if os.path.isdir(item):
-            result["directories"].append(item)
-        else:
-            result["files"].append(item)
-
-    return result
+def check_run():
+    return 1
--- rp_handler.py
+++ rp_handler.py
@@ -1,11 +1,32 @@
 import runpod
 import time
 import sys
-from .code.func import list_current_folder
+import os
+
+def list_current_folder():
+    current_dir = os.getcwd()
+    result = {
+        "current_directory": current_dir,
+        "directories": [],
+        "files": []
+    }
+
+    for item in os.listdir(current_dir):
+        if os.path.isdir(item):
+            result["directories"].append(item)
+        else:
+            result["files"].append(item)
+
+    return result
 
 def handler(event):
-
-    return list_current_folder()
+    a = list_current_folder()
+    try:
+        from code.func import check_run
+        b = check_run()
+        return a, b
+    except:    
+        return False, a
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler })
